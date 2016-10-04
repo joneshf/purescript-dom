a731cdaff37947727fc541fa7a6856ed6846b356
Ryan L. Bell <ryan.l.bell@gmail.com>
2016-10-04 15:28:50 -0400

    Fix style errors in Javascript file.
    Fixes CI build.

--- src/DOM/HTML/Event/DragEvent/DataTransfer.js
+++ src/DOM/HTML/Event/DragEvent/DataTransfer.js
@@ -1,13 +1,12 @@
 "use strict";
 
-exports.filesImpl = function(just) {
-  return function(nothing) {
-    return function(dataTransfer) {
+exports.filesImpl = function (just) {
+  return function (nothing) {
+    return function (dataTransfer) {
       if (dataTransfer.files) {
-	return just(dataTransfer.files);
-      }
-      else {
-	return nothing;
+        return just(dataTransfer.files);
+      } else {
+        return nothing;
       }
     };
   };
