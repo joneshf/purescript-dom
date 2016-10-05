db48b891e43b4a69f2b4f6369c84ec0d2caf3bfb
Ryan L. Bell <ryan.l.bell@gmail.com>
2016-10-04 22:03:36 -0400

    Use Nullable to handle foreign instances that may be null.
    Replaces the Maybe constructor passing I was using, following the
    convention already established else where.

--- src/DOM/HTML/Event/DragEvent/DataTransfer.js
+++ src/DOM/HTML/Event/DragEvent/DataTransfer.js
@@ -1,13 +1,5 @@
 "use strict";
 
-exports.filesImpl = function (just) {
-  return function (nothing) {
-    return function (dataTransfer) {
-      if (dataTransfer.files) {
-        return just(dataTransfer.files);
-      } else {
-        return nothing;
-      }
-    };
-  };
+exports.filesNullable = function (dataTransfer) {
+  return dataTransfer.files;
 };
--- src/DOM/HTML/Event/DragEvent/DataTransfer.purs
+++ src/DOM/HTML/Event/DragEvent/DataTransfer.purs
@@ -3,20 +3,19 @@ module DOM.HTML.Event.DragEvent.DataTransfer
   , files
   ) where
 
+import Prelude
 import DOM.File.Types (FileList)
-import Data.Maybe (Maybe(Just, Nothing))
+import Data.Maybe (Maybe)
+import Data.Nullable (Nullable, toMaybe)
 
 foreign import data DataTransfer :: *
 
 -- | Contains a list of all the local files available on the data transfer.
 -- | Empty if the drag operation doesn't involve dragging files.
 -- |
--- | It's possible that a drag operation may be null on some browsers. In
--- | these cases Nothing is returned.
+-- | It's possible that a drag operation may have null files, instead of an
+-- | empty file list. In these cases Nothing is returned.
 files :: DataTransfer -> Maybe FileList
-files = filesImpl Just Nothing
+files = toMaybe <$> filesNullable
 
-foreign import filesImpl :: (FileList -> Maybe FileList)
-			 -> Maybe FileList
-			 -> DataTransfer
-                         -> Maybe FileList
+foreign import filesNullable :: DataTransfer -> Nullable FileList
