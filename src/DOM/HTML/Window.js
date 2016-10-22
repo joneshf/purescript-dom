14b1660d29ac691e587cf90436075643d58706ea
Risto Stevcev <risto1@gmail.com>
2016-10-22 22:27:07 +0200

    Updated style for jshint

--- src/DOM/HTML/Window.js
+++ src/DOM/HTML/Window.js
@@ -30,53 +30,53 @@ exports.innerHeight = function (window) {
   };
 };
 
-exports.alert = function(window) {
-  return function(str) {
-    return function() {
+exports.alert = function (window) {
+  return function (str) {
+    return function () {
       window.alert(str);
       return {};
     };
   };
 };
 
-exports.close = function(window) {
-  return function() {
+exports.close = function (window) {
+  return function () {
     window.close();
     return {};
   };
 };
 
-exports.closed = function(window) {
-  return function() {
+exports.closed = function (window) {
+  return function () {
     return window.closed;
   };
 };
 
-exports.confirm = function(window) {
-  return function(str) {
-    return function() {
+exports.confirm = function (window) {
+  return function (str) {
+    return function () {
       return window.confirm(str);
     };
   };
 };
 
-exports.length = function(window) {
-  return function() {
+exports.length = function (window) {
+  return function () {
     return window.length;
   };
 };
 
-exports.minimize = function(window) {
-  return function() {
+exports.minimize = function (window) {
+  return function () {
     window.minimize();
     return {};
   };
 };
 
-exports.moveBy = function(window) {
-  return function(xDelta) {
-    return function(yDelta) {
-      return function() {
+exports.moveBy = function (window) {
+  return function (xDelta) {
+    return function (yDelta) {
+      return function () {
         window.moveBy(xDelta, yDelta);
         return {};
       };
@@ -84,10 +84,10 @@ exports.moveBy = function(window) {
   };
 };
 
-exports.moveTo = function(window) {
-  return function(width) {
-    return function(height) {
-      return function() {
+exports.moveTo = function (window) {
+  return function (width) {
+    return function (height) {
+      return function () {
         window.moveTo(width, height);
         return {};
       };
@@ -95,13 +95,13 @@ exports.moveTo = function(window) {
   };
 };
 
-exports._open = function(just) {
-  return function(nothing) {
-    return function(window) {
-      return function(url) {
-        return function(name) {
-          return function(features) {
-            return function() {
+exports._open = function (just) {
+  return function (nothing) {
+    return function (window) {
+      return function (url) {
+        return function (name) {
+          return function (features) {
+            return function () {
               var windowRef = window.open(url, name, features);
               return windowRef !== null ? just(windowRef) : nothing();
             };
@@ -112,30 +112,30 @@ exports._open = function(just) {
   };
 };
 
-exports.outerHeight = function(window) {
-  return function() {
+exports.outerHeight = function (window) {
+  return function () {
     return window.outerHeight;
   };
 };
 
-exports.outerWidth = function(window) {
-  return function() {
+exports.outerWidth = function (window) {
+  return function () {
     return window.outerWidth;
   };
 };
 
-exports.print = function(window) {
-  return function() {
+exports.print = function (window) {
+  return function () {
     window.print();
     return {};
   };
 };
 
-exports._prompt = function(just) {
-  return function(nothing) {
-    return function(window) {
-      return function(str) {
-        return function() {
+exports._prompt = function (just) {
+  return function (nothing) {
+    return function (window) {
+      return function (str) {
+        return function () {
           var response = window.prompt(str);
           return response !== null ? just(response) : nothing();
         };
@@ -144,10 +144,10 @@ exports._prompt = function(just) {
   };
 };
 
-exports.resizeBy = function(window) {
-  return function(xDelta) {
-    return function(yDelta) {
-      return function() {
+exports.resizeBy = function (window) {
+  return function (xDelta) {
+    return function (yDelta) {
+      return function () {
         window.resizeBy(xDelta, yDelta);
         return {};
       };
@@ -155,10 +155,10 @@ exports.resizeBy = function(window) {
   };
 };
 
-exports.resizeTo = function(window) {
-  return function(width) {
-    return function(height) {
-      return function() {
+exports.resizeTo = function (window) {
+  return function (width) {
+    return function (height) {
+      return function () {
         window.resizeTo(width, height);
         return {};
       };
@@ -166,22 +166,22 @@ exports.resizeTo = function(window) {
   };
 };
 
-exports.screenX = function(window) {
-  return function() {
+exports.screenX = function (window) {
+  return function () {
     return window.screenX;
   };
 };
 
-exports.screenY = function(window) {
-  return function() {
+exports.screenY = function (window) {
+  return function () {
     return window.screenY;
   };
 };
 
-exports.scroll = function(window) {
-  return function(xCoord) {
-    return function(yCoord) {
-      return function() {
+exports.scroll = function (window) {
+  return function (xCoord) {
+    return function (yCoord) {
+      return function () {
         window.scroll(xCoord, yCoord);
         return {};
       };
@@ -189,10 +189,10 @@ exports.scroll = function(window) {
   };
 };
 
-exports.scrollBy = function(window) {
-  return function(xCoord) {
-    return function(yCoord) {
-      return function() {
+exports.scrollBy = function (window) {
+  return function (xCoord) {
+    return function (yCoord) {
+      return function () {
         window.scrollBy(xCoord, yCoord);
         return {};
       };
@@ -200,14 +200,14 @@ exports.scrollBy = function(window) {
   };
 };
 
-exports.scrollX = function(window) {
-  return function() {
+exports.scrollX = function (window) {
+  return function () {
     return window.scrollX;
   };
 };
 
-exports.scrollY = function(window) {
-  return function() {
+exports.scrollY = function (window) {
+  return function () {
     return window.scrollY;
   };
 };
