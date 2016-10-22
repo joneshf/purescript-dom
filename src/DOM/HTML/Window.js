f9bbd37a32f240655b96b1027b04465a948a9a0c
Risto Stevcev <risto1@gmail.com>
2016-10-22 17:01:38 +0200

    Added explicit semicolons

--- src/DOM/HTML/Window.js
+++ src/DOM/HTML/Window.js
@@ -33,45 +33,45 @@ exports.innerHeight = function (window) {
 exports.alert = function(window) {
   return function(str) {
     return function() {
-      alert(str);
+      window.alert(str);
       return {};
-    }
-  }
-}
+    };
+  };
+};
 
 exports.close = function(window) {
   return function() {
     window.close();
     return {};
-  }
-}
+  };
+};
 
 exports.closed = function(window) {
   return function() {
     return window.closed;
-  }
-}
+  };
+};
 
 exports.confirm = function(window) {
   return function(str) {
     return function() {
       return window.confirm(str);
-    }
-  }
-}
+    };
+  };
+};
 
 exports.length = function(window) {
   return function() {
     return window.length;
-  }
-}
+  };
+};
 
 exports.minimize = function(window) {
   return function() {
     window.minimize();
     return {};
-  }
-}
+  };
+};
 
 exports.moveBy = function(window) {
   return function(xDelta) {
@@ -79,10 +79,10 @@ exports.moveBy = function(window) {
       return function() {
         window.moveBy(xDelta, yDelta);
         return {};
-      }
-    }
-  }
-}
+      };
+    };
+  };
+};
 
 exports.moveTo = function(window) {
   return function(width) {
@@ -90,10 +90,10 @@ exports.moveTo = function(window) {
       return function() {
         window.moveTo(width, height);
         return {};
-      }
-    }
-  }
-}
+      };
+    };
+  };
+};
 
 exports._open = function(just) {
   return function(nothing) {
@@ -104,32 +104,32 @@ exports._open = function(just) {
             return function() {
               var windowRef = window.open(url, name, features);
               return windowRef !== null ? just(windowRef) : nothing();
-            }
-          }
-        }
-      }
-    }
-  }
-}
+            };
+          };
+        };
+      };
+    };
+  };
+};
 
 exports.outerHeight = function(window) {
   return function() {
     return window.outerHeight;
-  }
-}
+  };
+};
 
 exports.outerWidth = function(window) {
   return function() {
     return window.outerWidth;
-  }
-}
+  };
+};
 
 exports.print = function(window) {
   return function() {
     window.print();
     return {};
-  }
-}
+  };
+};
 
 exports._prompt = function(just) {
   return function(nothing) {
@@ -138,11 +138,11 @@ exports._prompt = function(just) {
         return function() {
           var response = window.prompt(str);
           return response !== null ? just(response) : nothing();
-        }
-      }
-    }
-  }
-}
+        };
+      };
+    };
+  };
+};
 
 exports.resizeBy = function(window) {
   return function(xDelta) {
@@ -150,10 +150,10 @@ exports.resizeBy = function(window) {
       return function() {
         window.resizeBy(xDelta, yDelta);
         return {};
-      }
-    }
-  }
-}
+      };
+    };
+  };
+};
 
 exports.resizeTo = function(window) {
   return function(width) {
@@ -161,22 +161,22 @@ exports.resizeTo = function(window) {
       return function() {
         window.resizeTo(width, height);
         return {};
-      }
-    }
-  }
-}
+      };
+    };
+  };
+};
 
 exports.screenX = function(window) {
   return function() {
     return window.screenX;
-  }
-}
+  };
+};
 
 exports.screenY = function(window) {
   return function() {
     return window.screenY;
-  }
-}
+  };
+};
 
 exports.scroll = function(window) {
   return function(xCoord) {
@@ -184,10 +184,10 @@ exports.scroll = function(window) {
       return function() {
         window.scroll(xCoord, yCoord);
         return {};
-      }
-    }
-  }
-}
+      };
+    };
+  };
+};
 
 exports.scrollBy = function(window) {
   return function(xCoord) {
@@ -195,18 +195,19 @@ exports.scrollBy = function(window) {
       return function() {
         window.scrollBy(xCoord, yCoord);
         return {};
-      }
-    }
-  }
-}
+      };
+    };
+  };
+};
 
 exports.scrollX = function(window) {
   return function() {
     return window.scrollX;
-  }
-}
+  };
+};
+
 exports.scrollY = function(window) {
   return function() {
     return window.scrollY;
-  }
-}
+  };
+};
