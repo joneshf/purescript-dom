6b3813cd57874076c5d4681940e93e82f80a065c
Tom <tomjharding@live.co.uk>
2017-07-16 19:25:03 +0100

    Update toArray to use FFI
    It's not great, but it's going to be more performant and just as
    "correct" (for some definition of correctness).

--- src/DOM/Node/HTMLCollection.purs
+++ src/DOM/Node/HTMLCollection.purs
@@ -5,16 +5,17 @@ module DOM.Node.HTMLCollection
   ) where
 
 import Prelude
-import Control.Monad.Eff (Eff, forE)
-import Control.Monad.ST (ST)
-import Data.Array.ST (emptySTArray, pushSTArray, freeze)
-import Data.Maybe (Maybe(..))
+import Control.Monad.Eff (Eff)
+import Data.Maybe (Maybe)
 import Data.Nullable (Nullable, toMaybe)
 import DOM (DOM)
 import DOM.Node.Types (Element, HTMLCollection, ElementId)
 
 -- | The number of elements in a HTMLCollection.
 foreign import length :: forall eff. HTMLCollection -> Eff (dom :: DOM | eff) Int
+
+-- | The elements of an HTMLCollection represented in an array.
+foreign import toArray :: forall eff. HTMLCollection -> Eff (dom :: DOM | eff) (Array Element)
 
 -- | The element in a HTMLCollection at the specified index, or Nothing if no such
 -- | element exists.
@@ -29,18 +30,3 @@ namedItem :: forall eff. ElementId -> HTMLCollection -> Eff (dom :: DOM | eff) (
 namedItem id = map toMaybe <<< _namedItem id
 
 foreign import _namedItem :: forall eff. ElementId -> HTMLCollection -> Eff (dom :: DOM | eff) (Nullable Element)
-
--- | The elements of an HTMLCollection represented in an array.
-toArray :: forall eff h. HTMLCollection -> Eff (dom :: DOM, st :: ST h | eff) (Array Element)
-toArray collection = do
-  result <- emptySTArray
-  len <- length collection
-
-  forE 0 len \i -> do
-    next <- item i collection
-
-    void $ case next of
-      Just elem -> pushSTArray result elem
-      Nothing   -> pure 0
-
-  freeze result
