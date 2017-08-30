3b6eca2a15c956a5171b7ea2fec9fd1cbda66b81
Tom Harding <tom.harding@habito.com>
2017-08-30 14:52:11 +0100

    --wip-- [skip ci]

--- src/DOM/XHR/FormData.purs
+++ src/DOM/XHR/FormData.purs
@@ -1,8 +1,5 @@
 module DOM.XHR.FormData
 	( FormDataValue(..)
-	, appendString
-	, appendWithName
-	, newFormData
 	, toFormData
 	) where
 
@@ -14,20 +11,12 @@ import Data.Foldable (class Foldable, for_)
 import Data.Tuple (Tuple(..))
 import DOM (DOM)
 import DOM.File.Types (Blob, File)
-import DOM.XHR.Types (FormData)
-
--- | Possible values of a `FormData`.
-data FormDataValue
-  = FormDataString String
-  | FormDataFile String File -- ^ File name and `File` object.
-  | FormDataBlob String Blob -- ^ Blob name and `Blob` object.
+import DOM.XHR.FormData.ST (newFormData, appendString, appendWithName, freeze, thaw)
+import DOM.XHR.Types (FormData, FormDataValue)
 
 -- | Convert an associated array of keys and values to a `FormData`.
-toFormData :: forall eff f h.
-              Foldable f
-           => f (Tuple String FormDataValue)
-           -> Eff (dom :: DOM, st :: ST h | eff) FormData
-toFormData dat = do
+toFormData :: forall eff f h. Foldable f => f (Tuple String FormDataValue) -> FormData
+toFormData dat = runPure $ runST $ do
   result <- newFormData
 
   for_ dat $ case _ of
@@ -35,31 +24,4 @@ toFormData dat = do
     Tuple key (FormDataFile name val) -> appendWithName result key val name
     Tuple key (FormDataBlob name val) -> appendWithName result key val name
 
-  pure result
-
-foreign import newFormData :: forall eff h. Eff (dom :: DOM, st :: ST h | eff) FormData
-
-foreign import appendStringImpl
-  :: forall eff h.
-     EffFn3 (dom :: DOM, st :: ST h | eff) FormData String String Unit
-
--- | Append a string value to a FormData object.
-appendString :: forall h eff.
-                FormData
-             -> String
-             -> String
-             -> Eff (dom :: DOM, st :: ST h | eff) Unit
-appendString = runEffFn3 appendStringImpl
-
-foreign import appendWithNameImpl
-  :: forall a h eff.
-     EffFn4 (dom :: DOM, st :: ST h | eff) FormData String a String Unit
-
--- | Append a body to a FormData object, supplying a name for the file
-appendWithName :: forall a h eff.
-                  FormData
-                -> String
-                -> a
-                -> String
-                -> Eff (dom :: DOM, st :: ST h | eff) Unit
-appendWithName = runEffFn4 appendWithNameImpl
+  freeze result
--- src/DOM/XHR/Types.purs
+++ src/DOM/XHR/Types.purs
@@ -4,3 +4,9 @@ foreign import data XMLHttpRequestEventTarget :: Type
 foreign import data XMLHttpRequestUpload :: Type
 foreign import data XMLHttpRequest :: Type
 foreign import data FormData :: Type
+
+-- | Possible values of a `FormData`.
+data FormDataValue
+  = FormDataString String
+  | FormDataFile String File -- ^ File name and `File` object.
+  | FormDataBlob String Blob -- ^ Blob name and `Blob` object.
